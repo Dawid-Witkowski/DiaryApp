5054cd5ebdaa0f2d716cd23baf487f81eb096ca7
Winged Dev <98784426+dawciu0001@users.noreply.github.com>
2023-07-18 20:03:49 +0200

    75 apply visiblity modifiers

--- app/src/main/java/com/stevdzasan/diaryapp/MainActivity.kt
+++ app/src/main/java/com/stevdzasan/diaryapp/MainActivity.kt
@@ -5,19 +5,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.core.net.toUri
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
-import com.google.firebase.FirebaseApp
-import com.stevdzasan.diaryapp.data.database.ImageToDeleteDao
-import com.stevdzasan.diaryapp.data.database.ImageToUploadDao
-import com.stevdzasan.diaryapp.navigation.Screen
-import com.stevdzasan.diaryapp.navigation.SetupNavGraph
+import com.example.mongo.database.entity.ImageToDelete
+import com.example.mongo.database.entity.ImageToUpload
 import com.example.ui.theme.DiaryAppTheme
-import com.stevdzasan.diaryapp.util.Constants.APP_ID
-import com.stevdzasan.diaryapp.util.retryDeletingImageFromFirebase
-import com.stevdzasan.diaryapp.util.retryUploadingImageToFirebase
+import com.example.util.Constants.APP_ID
+import com.example.util.Screen
+import com.google.firebase.FirebaseApp
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storageMetadata
+import com.stevdzasan.diaryapp.navigation.SetupNavGraph
 import dagger.hilt.android.AndroidEntryPoint
 import io.realm.kotlin.mongodb.App
 import kotlinx.coroutines.CoroutineScope
@@ -30,9 +31,9 @@ import javax.inject.Inject
 class MainActivity : ComponentActivity() {
 
     @Inject
-    lateinit var imageToUploadDao: ImageToUploadDao
+    lateinit var imageToUploadDao: com.example.mongo.database.ImageToUploadDao
     @Inject
-    lateinit var imageToDeleteDao: ImageToDeleteDao
+    lateinit var imageToDeleteDao: com.example.mongo.database.ImageToDeleteDao
     private var keepSplashOpened = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,8 +66,8 @@ class MainActivity : ComponentActivity() {
 
 private fun cleanupCheck(
     scope: CoroutineScope,
-    imageToUploadDao: ImageToUploadDao,
-    imageToDeleteDao: ImageToDeleteDao
+    imageToUploadDao: com.example.mongo.database.ImageToUploadDao,
+    imageToDeleteDao: com.example.mongo.database.ImageToDeleteDao
 ) {
     scope.launch(Dispatchers.IO) {
         val result = imageToUploadDao.getAllImages()
@@ -98,4 +99,25 @@ private fun getStartDestination(): String {
     val user = App.create(APP_ID).currentUser
     return if (user != null && user.loggedIn) Screen.Home.route
     else Screen.Authentication.route
+}
+
+fun retryUploadingImageToFirebase(
+    imageToUpload: ImageToUpload,
+    onSuccess: () -> Unit
+) {
+    val storage = FirebaseStorage.getInstance().reference
+    storage.child(imageToUpload.remoteImagePath).putFile(
+        imageToUpload.imageUri.toUri(),
+        storageMetadata { },
+        imageToUpload.sessionUri.toUri()
+    ).addOnSuccessListener { onSuccess() }
+}
+
+fun retryDeletingImageFromFirebase(
+    imageToDelete: ImageToDelete,
+    onSuccess: () -> Unit
+) {
+    val storage = FirebaseStorage.getInstance().reference
+    storage.child(imageToDelete.remoteImagePath).delete()
+        .addOnSuccessListener { onSuccess() }
 }
---
+++ feature/write/src/main/java/com/example/write/navigation/WriteNavigation.kt
@@ -0,0 +1,85 @@
+package com.example.write.navigation
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
+import com.example.util.Screen
+import com.example.util.model.Mood
+import com.example.write.WriteScreen
+import com.example.write.WriteViewModel
+
+@OptIn(ExperimentalFoundationApi::class)
+fun NavGraphBuilder.writeRoute(navigateBack: () -> Unit) {
+    composable(
+        route = Screen.Write.route,
+        arguments = listOf(navArgument(name = WRITE_SCREEN_ARGUMENT_KEY) {
+            type = NavType.StringType
+            nullable = true
+            defaultValue = null
+        })
+    ) {
+        val viewModel: WriteViewModel = hiltViewModel()
+        val context = LocalContext.current
+        val uiState = viewModel.uiState
+        val galleryState = viewModel.galleryState
+        val pagerState = rememberPagerState()
+        val pageNumber by remember { derivedStateOf { pagerState.currentPage } }
+
+        WriteScreen(
+            uiState = uiState,
+            pagerState = pagerState,
+            galleryState = galleryState,
+            moodName = { Mood.values()[pageNumber].name },
+            onTitleChanged = { viewModel.setTitle(title = it) },
+            onDescriptionChanged = { viewModel.setDescription(description = it) },
+            onDateTimeUpdated = { viewModel.updateDateTime(zonedDateTime = it) },
+            onBackPressed = navigateBack,
+            onDeleteConfirmed = {
+                viewModel.deleteDiary(
+                    onSuccess = {
+                        android.widget.Toast.makeText(
+                            context,
+                            "Deleted",
+                            android.widget.Toast.LENGTH_SHORT
+                        ).show()
+                        navigateBack()
+                    },
+                    onError = { message ->
+                        android.widget.Toast.makeText(
+                            context,
+                            message,
+                            android.widget.Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                )
+            },
+            onSaveClicked = {
+                viewModel.upsertDiary(
+                    diary = it.apply { mood = Mood.values()[pageNumber].name },
+                    onSuccess = navigateBack,
+                    onError = { message ->
+                        android.widget.Toast.makeText(
+                            context,
+                            message,
+                            android.widget.Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                )
+            },
+            onImageSelect = {
+                val type = context.contentResolver.getType(it)?.split("/")?.last() ?: "jpg"
+                viewModel.addImage(image = it, imageType = type)
+            },
+            onImageDeleteClicked = { galleryState.removeImage(it) }
+        )
+    }
+}
